a4e0058771dd98f9861c66c322a76997430e4560
pramod-thupaki <thupakip@hotmail.com>
2021-06-08 16:30:52 -0700

    parallelize download

--- downloader/utils/downloader_wrapper.py
+++ downloader/utils/downloader_wrapper.py
@@ -1,8 +1,9 @@
 import pdfkit
 import json
+from multiprocessing import Process
 
-def download_ckan_pdf(ckan_url='https://catalogue.cioospacific.ca/dataset/', ckan_id='f239e86c-38d1-4fb2-8b17-acaed794221c', 
-                        pdf_filename='ckan.pdf'):
+
+def download_ckan_pdf(ckan_url=None, ckan_id=None, pdf_filename=None):
     download_url = ckan_url+ckan_id
     if pdfkit.from_url(download_url, pdf_filename):
         return 0
@@ -12,11 +13,23 @@ def download_ckan_pdf(ckan_url='https://catalogue.cioospacific.ca/dataset/', cka
 
 
 def parallel_downloader(json_blob=None, output_folder='../'):
-    pass
+    for filtered_result in json_blob['cache_filtered']:
+        erddap_url = filtered_result['erddap_url']
+        ckan_url = filtered_result['ckan_url']
+        ckan_id = filtered_result['ckan_id']
+        ckan_filename = '{}{}_{}.pdf'.format(output_folder, filtered_result['dataset_id'], erddap_url.split('/')[2].replace('.','_') )
+        pid = Process(target=download_ckan_pdf, args=(ckan_url, ckan_id, ckan_filename))
+        pid.start()
+        pid.join()
+
+        # call jessy's code here to download data from erddap
+
 
 
 
 if __name__ == "__main__":
     with open('sample_blob.json','r') as fid:
         json_blob = json.load(fid)
-    print(json_blob)
+    parallel_downloader(json_blob=json_blob, output_folder='./')
+
+    
