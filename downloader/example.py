d186175a3967848e25b769fe897e88ef67536225
pramod-thupaki <thupakip@hotmail.com>
2021-06-09 16:19:43 -0700

    download to randomly named sub-folders

--- downloader/example.py
+++ downloader/example.py
@@ -3,7 +3,8 @@ from erddap_downloader import downloader_wrapper
 import json 
 
 if __name__ == "__main__":
-    with open('test/sample_blob.json','r') as fid:
+    with open('test/test_multiple_dataset_query.json','r') as fid:
         json_blob = json.load(fid)
 
+# print(downloader_wrapper.gen_folder_name())
 downloader_wrapper.parallel_downloader(json_blob=json_blob, output_folder='./out/')
