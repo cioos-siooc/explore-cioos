3deefc6077c1a6853e9ee3486d0735f2f9b82b13
Jessy Barrette <jessy.barrette@hakai.org>
2021-06-09 17:20:35 -0700

    Indent downloader_wrapper to make it compatible with windows example.py

--- downloader/example.py
+++ downloader/example.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     with open('test/test_multiple_dataset_query.json','r') as fid:
         json_blob = json.load(fid)
 
-# print(downloader_wrapper.gen_folder_name())
-downloader_wrapper.parallel_downloader(json_blob=json_blob, output_folder='./out/')
+    # print(downloader_wrapper.gen_folder_name())
+    downloader_wrapper.parallel_downloader(json_blob=json_blob, output_folder='./out/')
