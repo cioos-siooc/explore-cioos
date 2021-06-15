6cf27ee20e36621e6a7977b96be529f26addbd61
Jessy Barrette <jessy.barrette@hakai.org>
2021-06-15 12:12:56 -0700

    Fix downloader_wrapper and format

--- downloader/erddap_downloader/downloader_wrapper.py
+++ downloader/erddap_downloader/downloader_wrapper.py
@@ -5,7 +5,6 @@ import os
 import zipfile
 import random
 
-
 if os.name == 'nt':
     path_wkthmltopdf = b'C:\Program Files\wkhtmltopdf\\bin\wkhtmltopdf.exe'
     config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
@@ -21,14 +20,14 @@ def gen_folder_name(fpath='../'):
             # Considering only upper and lowercase letters
             random_integer = random.randint(97, 97 + 26 - 1)
             random_string += (chr(random_integer))
-        return random_string 
+        return random_string
 
     while True:
         fname = gen_string(10)
         if not os.path.exists(os.path.join(fpath, fname)):
             break
     return os.path.join(fpath, fname)
-            
+
 
 def download_ckan_pdf(ckan_url=None, ckan_id=None, pdf_filename=None):
     download_url = ckan_url + ckan_id
@@ -64,10 +63,8 @@ def parallel_downloader(json_blob=None, output_folder="../"):
         pid.start()
         pid.join()
     # zip files in folder
-    if os.name is not 'nt':
+    if os.name != 'nt':
         retval = os.system('zip -r {}.zip {}'.format(output_folder, output_folder))
-        if retval==0:
-            os.system('rm -r {}'.format(output_folder) )
-            
+        if retval == 0:
+            os.system('rm -r {}'.format(output_folder))
     return '{}.zip'.format(output_folder)
-
