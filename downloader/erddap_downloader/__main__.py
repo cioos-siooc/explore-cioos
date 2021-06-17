3dbd8a441c06876b0649254bb5859e369a7fcab3
Pramod Thupaki <thupakip@hotmail.com>
2021-06-17 12:51:30 -0700

    remove downloader.py
    #59

--- downloader/erddap_downloader/__main__.py
+++ downloader/erddap_downloader/__main__.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     with open(json_query) as fid:
         json_blob = json.load(fid)
 
-    json_blob["create_pdf"] = pdf_option.strip.upper() == "Y"
+    json_blob["create_pdf"] = pdf_option.strip().upper() == "Y"
 
     # Run query in parallel mode
     downloader_wrapper.parallel_downloader(
