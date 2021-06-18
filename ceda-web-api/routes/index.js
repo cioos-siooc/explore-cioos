fe4487feb09c5e8288766c810b4e1ab54f1045a1
Nate <nate.rosenstock@hakai.org>
2021-06-18 11:22:31 -0700

    add organization filter

--- ceda-web-api/routes/index.js
+++ ceda-web-api/routes/index.js
@@ -1,5 +1,14 @@
 var express = require("express");
 var router = express.Router();
+const db = require("../db");
+const createDBFilter = require("../utils/dbFilter");
+
+// These routes are too small to have their own files
+
+// gets all of them, not dependant on query
+router.get("/organizations", async function (req, res, next) {
+  res.send(await db("cioos_api.organizations"));
+});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
