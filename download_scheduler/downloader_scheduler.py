225c9df1183bb4c895820fef08b357dbb1ed4898
Nate <nate.rosenstock@hakai.org>
2021-06-21 09:51:34 -0700

    uncomment failed status line, run black and isort

--- download_scheduler/downloader_scheduler.py
+++ download_scheduler/downloader_scheduler.py
@@ -1,11 +1,11 @@
-from re import L
-from erddap_downloader import downloader_wrapper
-
 import configparser
-from sqlalchemy import create_engine
 # from sqlalchemy import JSON, Text
 import json
 import time
+from re import L
+
+from erddap_downloader import downloader_wrapper
+from sqlalchemy import create_engine
 
 config = configparser.ConfigParser()
 config.read(".env")
@@ -17,53 +17,63 @@ data_base_link = (
 
 engine = create_engine(data_base_link)
 
-output_folder = './out/'
+output_folder = "./out/"
 create_pdf = False
+
 
 def get_a_download_job():
     """
     :return: Latest timestamp avaiable on the CIOOS Bayne Sound database
     """
-    
-    rs = engine.execute("SELECT * FROM cioos_api.download_jobs WHERE status='open' ORDER BY time ASC LIMIT 1")
+
+    rs = engine.execute(
+        "SELECT * FROM cioos_api.download_jobs WHERE status='open' ORDER BY time ASC LIMIT 1"
+    )
     return rs.fetchone()
 
+
 def run_download(row):
-    pk = row['pk']
+    pk = row["pk"]
 
     # Update status
-    update_download_jobs(pk,{"status":"downloading"})
+    update_download_jobs(pk, {"status": "downloading"})
 
     # Run Download
     try:
-        downloader_input = json.loads(row['downloader_input'])
+        downloader_input = json.loads(row["downloader_input"])
         print(downloader_input)
         # Run query in parallel mode
         result = downloader_wrapper.parallel_downloader(
-            json_blob=downloader_input, output_folder=output_folder, create_pdf=create_pdf
+            json_blob=downloader_input,
+            output_folder=output_folder,
+            create_pdf=create_pdf,
         )
 
         # Download Completed Update Status
-        update_download_jobs(pk,{"status":"completed","downloader_output":str(result)})
-       
+        update_download_jobs(
+            pk, {"status": "completed", "downloader_output": str(result)}
+        )
+
     except Exception as e:
-        # update_download_jobs(pk,{"status":"failed", "downloader_output":str(e)})
-        update_download_jobs(pk,{"status":"open", "downloader_output":str(e)})
+        update_download_jobs(pk, {"status": "failed", "downloader_output": str(e)})
         print(e)
 
-def update_download_jobs(pk,row):
-    for key,value in row.items():
-        engine.execute(f"UPDATE cioos_api.download_jobs SET {key}='{value}' WHERE PK={pk}")
+
+def update_download_jobs(pk, row):
+    for key, value in row.items():
+        engine.execute(
+            f"UPDATE cioos_api.download_jobs SET {key}='{value}' WHERE PK={pk}"
+        )
+
 
 if __name__ == "__main__":
     while True:
         row = get_a_download_job()
 
         if row:
-            print("Starting job:",row['pk'])
+            print("Starting job:", row["pk"])
             run_download(row)
         else:
             print("No jobs")
-        
+
         time.sleep(2)
-    
--- download_scheduler/setup.py
+++ download_scheduler/setup.py
@@ -2,14 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='downloader_scheduler',
-      version='0.1',
-      description='',
-      url='',
-      packages=['downloader_scheduler'],
-      install_requires=[
-            'sqlalchemy',
-            'psycopg2',
-
-      ]
-      )
+setup(
+    name="downloader_scheduler",
+    version="0.1",
+    description="",
+    url="",
+    packages=["downloader_scheduler"],
+    install_requires=[
+        "sqlalchemy",
+        "psycopg2",
+    ],
+)
