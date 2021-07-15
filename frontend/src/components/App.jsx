49ead4137086fef3f611430c602a0694ea2c6bc2
Nate <nate.rosenstock@hakai.org>
2021-07-15 09:56:15 -0700

    fix map I broke

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import Controls from "./Controls/Controls.jsx";
-import './styles.css'
+import CIOOSMap from "./Map/Map.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn: "https://ccb1d8806b1c42cb83ef83040dc0d7c0@o56764.ingest.sentry.io/5863595",
