d55ebe006b6ec6b4a5d818b8d464c3fe46ab8a49
Jessy Barrette <jessy.barrette@hakai.org>
2021-12-13 11:08:31 -0800

    Fix dataset size estimator when one single dataset is retrieved

--- scraper/erddap_estimate/__main__.py
+++ scraper/erddap_estimate/__main__.py
@@ -143,7 +143,7 @@ def get_dataset_size(datasets, standard_name=[""]):
     sql_query = f"""
     SELECT erddap_url ,dataset_id ,"type",COUNT("type") FROM {schema}.erddap_variables 
     WHERE  ( 
-        (erddap_url,dataset_id) in {tuple(datasets.index.values.tolist())}
+        (erddap_url,dataset_id) in {'('+str(datasets.index.values[0])+')' if len(datasets.index.values)==1 else tuple(datasets.index.values)}
         AND (
                 variable in ('time','latitude','longitude','depth') 
                 OR cf_role is not NULL 
