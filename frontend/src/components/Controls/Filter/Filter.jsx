cdd00a2e4057688b0caf97c30ac19a6d37aeb52e
JorinHakai <jorin.weatherston@hakai.org>
2022-01-07 08:04:51 -0800

    only one filter opens at a time now

--- frontend/src/components/Controls/Filter/Filter.jsx
+++ frontend/src/components/Controls/Filter/Filter.jsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
-import { useState } from 'react'
+import * as _ from 'lodash'
+import { useState, useEffect } from 'react'
 import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { ChevronCompactDown, ChevronCompactUp, QuestionCircle } from 'react-bootstrap-icons'
 import { abbreviateString } from '../../../utilities'
 
 import './styles.css'
 
-export default function Filter({ badgeTitle, optionsSelected, setOptionsSelected, tooltip, icon, children }) {
+export default function Filter({ badgeTitle, optionsSelected, setOptionsSelected, tooltip, icon, controlled, openFilter, setOpenFilter, filterName, children }) {
   // Open/Closed state for filter dropdown
-  const [filterOpen, setFilterOpen] = useState(false)
+  const [filterOpen, setFilterOpen] = useState(controlled ? openFilter : false)
 
   function resetDefaults(optionsSelected, setOptionsSelected) {
     let copyOfOptionsSelected = { ...optionsSelected }
@@ -16,11 +17,19 @@ export default function Filter({ badgeTitle, optionsSelected, setOptionsSelected
     setOptionsSelected(copyOfOptionsSelected)
   }
 
+  useEffect(() => {
+    controlled ? setFilterOpen(openFilter) : _.noop()
+  }, [openFilter])
+
   // Using tabIndex to enable onBlur() focus loss capturing: https://stackoverflow.com/a/37491578
   return (
     <div className='filter'>
       {/* tabIndex={0} onBlur={() => setFilterOpen(false)}> */}
-      <div className='filterHeader' onClick={() => setFilterOpen(!filterOpen)} >
+      <div className='filterHeader' onClick={() => {
+        setFilterOpen(!filterOpen)
+        if (controlled) setOpenFilter(filterName)
+      }}
+      >
         {tooltip &&
           <OverlayTrigger
             key={badgeTitle}
@@ -40,7 +49,7 @@ export default function Filter({ badgeTitle, optionsSelected, setOptionsSelected
         </div>
         {filterOpen ? <ChevronCompactUp /> : <ChevronCompactDown />}
       </div>
-      {filterOpen &&
+      {(controlled ? filterOpen && openFilter : filterOpen) &&
         <div className='filterOptions'>
           {children}
           <button onClick={() => resetDefaults(optionsSelected, setOptionsSelected)}>
