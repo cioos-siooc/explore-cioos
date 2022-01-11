21e6938204da21231c8231d8e46e4e370d1ff657
JorinHakai <jorin.weatherston@hakai.org>
2022-01-10 16:14:22 -0800

    Added pointDetails component to handle querying and presenting point info, and returning user selection of data to download

--- frontend/src/components/Controls/PointDetails/PointDetails.jsx
+++ frontend/src/components/Controls/PointDetails/PointDetails.jsx
@@ -1,10 +1,35 @@
 import * as React from 'react'
+import { useState, useEffect } from 'react'
+
+import { server } from '../../../config'
+
 import './styles.css'
 
-export default function PointDetails({ children }) {
+export default function PointDetails({ pointPKs, setPointsToDownload }) {
+  const [pointsData, setPointsData] = useState([])
+
+  useEffect(() => {
+    if (pointPKs && pointPKs.length !== 0) {
+      console.log(pointPKs)
+      fetch(`${server}/pointQuery/${pointPKs.join(',')}`).then(response => {
+        if (response.ok) {
+          response.json().then(data => {
+            setPointsData(data)
+          })
+        }
+      })
+    }
+  }, [pointPKs])
+
   return (
     <div>
-      {children}
+      These are the PKs of points that have been selected.
+      <span>
+        {JSON.stringify(pointPKs)}
+      </span>
+      <span>
+        {JSON.stringify(pointsData)}
+      </span>
     </div>
   )
 }
