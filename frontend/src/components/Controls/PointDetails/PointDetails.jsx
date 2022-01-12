a9377ce4dc7e9a40fa393c6cb66213f3916550af
JorinHakai <jorin.weatherston@hakai.org>
2022-01-12 11:29:04 -0800

    Added titles to table columns and selection toggling for all and individual datasets, changed selection details width

--- frontend/src/components/Controls/PointDetails/PointDetails.jsx
+++ frontend/src/components/Controls/PointDetails/PointDetails.jsx
@@ -8,7 +8,10 @@ import { server } from '../../../config'
 import './styles.css'
 
 export default function PointDetails({ pointPKs, setPointsToDownload }) {
+  const [selectAll, setSelectAll] = useState(true)
   const [pointsData, setPointsData] = useState([])
+  const [selectedDataset, setSelectedDataset] = useState()
+
   const checkColWidth = '33px'
   const titleColWidth = '171px'
   const typeColWidth = '100px'
@@ -21,12 +24,41 @@ export default function PointDetails({ pointPKs, setPointsToDownload }) {
       fetch(`${server}/pointQuery/${pointPKs.join(',')}`).then(response => {
         if (response.ok) {
           response.json().then(data => {
-            setPointsData(data)
+
+            setPointsData(data.map(point => {
+              return {
+                ...point,
+                selected: true
+              }
+            }))
           })
         }
       })
     }
   }, [pointPKs])
+
+  function handleDatasetSelection(point) {
+    let dataset = pointsData.filter((p) => p.dataset_id === point.dataset_id)[0]
+    dataset.selected = !point.selected
+    const result = pointsData.map((p) => {
+      if (p.dataset_id === point.dataset_id) {
+        return dataset
+      } else {
+        return p
+      }
+    })
+    setPointsData(result)
+  }
+
+  function handleSelectAll() {
+    setPointsData(pointsData.map(p => {
+      return {
+        ...p,
+        selected: !selectAll
+      }
+    }))
+    setSelectAll(!selectAll)
+  }
 
   return (
     <div className='pointDetails'>
@@ -34,23 +66,22 @@ export default function PointDetails({ pointPKs, setPointsToDownload }) {
         <Table striped hover>
           <thead>
             <tr>
-              <th style={{ "width": checkColWidth }} title='Select all'><CheckSquare /></th>
-              <th style={{ "width": titleColWidth }}>Title</th>
-              <th style={{ "width": typeColWidth }}>Type</th>
-              <th style={{ "width": pointsColWidth }}>Points</th>
-              <th style={{ "width": sizeColWidth }}>Size</th>
+              <th style={{ "width": checkColWidth }} title='Select all' onClick={() => handleSelectAll()}>{selectAll ? <CheckSquare /> : <Square />}</th>
+              <th style={{ "width": titleColWidth }} title='Dataset title'>Title</th>
+              <th style={{ "width": typeColWidth }} title='Dataset type'>Type</th>
+              <th style={{ "width": pointsColWidth }} title='Number of points in dataset'>Points</th>
+              <th style={{ "width": sizeColWidth }} title='Approximate dataset size in megabytes'>Size</th>
             </tr>
           </thead>
           <tbody>
             {pointsData.map((point, index) => {
-              { console.log(pointsData) }
               return (
-                <tr key={index}>
-                  <td style={{ "width": checkColWidth }}><CheckSquare /></td>
-                  <td style={{ "width": titleColWidth }}>{point.title}</td>
-                  <td style={{ "width": typeColWidth }}>A/B/C/D</td>
-                  <td style={{ "width": pointsColWidth }}>{point.profiles.length}</td>
-                  <td style={{ "width": sizeColWidth }}>{Math.floor(point.profiles.length * 0.1)} MB</td>
+                <tr key={index} onClick={() => { }} title='Open dataset details'>
+                  <td style={{ "width": checkColWidth }} onClick={() => handleDatasetSelection(point)} title='Select dataset for download'>{point.selected ? <CheckSquare /> : <Square />}</td>
+                  <td style={{ "width": titleColWidth }} title='Dataset title'>{point.title}</td>
+                  <td style={{ "width": typeColWidth }} title='Dataset type'>A/B/C/D</td>
+                  <td style={{ "width": pointsColWidth }} title='Number of points in dataset'>{point.profiles.length}</td>
+                  <td style={{ "width": sizeColWidth }} title='Approximate dataset size in megabytes'>{Math.floor(point.profiles.length * 0.1)} MB</td>
                 </tr>
               )
             })}
