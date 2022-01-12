104f27ca972d57437655e62934c0b84dd0eaa33a
JorinHakai <jorin.weatherston@hakai.org>
2022-01-12 12:40:32 -0800

    added DatasetInspector

---
+++ frontend/src/components/Controls/DatasetInspector/DatasetInspector.jsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import { ChevronCompactLeft } from 'react-bootstrap-icons'
+import { Container, Table } from 'react-bootstrap'
+
+import './styles.css'
+
+export default function DatasetInspector({ dataset, setInspectDataset }) {
+
+  return (
+    <div className='datasetInspector'>
+      <div onClick={() => setInspectDataset()}>
+        <ChevronCompactLeft /> Back
+      </div>
+      <div>
+        {console.log(dataset)}
+        <Container style={{ pointerEvents: 'auto', margin: '10px 0px 10px 0px' }}>
+          <hr />
+          <h6>
+            Title
+          </h6>
+          <div>
+            {dataset.title}
+          </div>
+          <hr />
+          <h6>
+            Organizations
+          </h6>
+          <div>
+            {dataset.parties}
+          </div>
+          <hr />
+          <h6>
+            Ocean Variables
+          </h6>
+          <div>
+            {dataset.eovs.map((eov, index) => ' ' + eov).join(',')}
+          </div>
+          <hr />
+          <h6>
+            Points ({dataset && dataset.profiles && dataset.profiles.length} profiles)
+          </h6>
+        </Container>
+        <Table className='profileTable' striped bordered size="sm">
+          <thead>
+            <tr>
+              <th>Profile ID</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>Start Depth</th>
+              <th>End Depth</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataset.profiles.map((profile, index) => {
+              return (
+                <tr key={index}>
+                  <td>{profile.profile_id}</td>
+                  <td>{new Date(profile.time_min).toLocaleDateString()}</td>
+                  <td>{new Date(profile.time_max).toLocaleDateString()}</td>
+                  <td>{profile.depth_min < Number.EPSILON ? 0 : profile.depth_min > 15000 ? 'too big' : profile.depth_min.toFixed(1)}</td>
+                  <td>{profile.depth_max < Number.EPSILON ? 0 : profile.depth_max > 15000 ? 'too big' : profile.depth_max.toFixed(1)}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  )
+
+}
