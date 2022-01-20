ddc8c0c3ea5270f468bf8dd3480cd9f1f2ccd2ef
JorinHakai <jorin.weatherston@hakai.org>
2022-01-19 16:09:45 -0800

    Added logo to upper right corner

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -49,13 +49,17 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
             loader: 'file-loader',
           },
         ],
       },
+      // {
+      //   test: /\.svg$/,
+      //   loader: 'svg-inline-loader'
+      // }
     ],
   },
   plugins: [
