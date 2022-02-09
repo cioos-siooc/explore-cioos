4a7e171bd510be59de68391f43b05a60882e61de
Nate <nate.rosenstock@hakai.org>
2022-02-09 14:28:20 -0800

    Add query validation, separate endpoint files, add comments
    missed one

--- web-api/routes/jobs.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const db = require("../db");
-
-router.get("/", async function (req, res, next) {
-  res.send(await db("cioos_api.download_jobs").orderBy("time", "desc"));
-});
-
-module.exports = router;
