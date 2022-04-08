2375b5718979914a62117f9397a17aa99718949f
JorinHakai <jorin.weatherston@hakai.org>
2022-04-08 15:28:31 -0700

    IntroModal translated

--- frontend/src/components/Controls/IntroModal/IntroModal.jsx
+++ frontend/src/components/Controls/IntroModal/IntroModal.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 import { Container, Modal, Col, Row } from 'react-bootstrap'
 import { InfoSquare } from 'react-bootstrap-icons'
 import { useTranslation } from 'react-i18next'
-
 import './styles.css'
 
 export default function IntroModal({ intialOpenState }) {
@@ -18,36 +17,39 @@ export default function IntroModal({ intialOpenState }) {
       case 'filter':
         return (
           <div className='stepInfo'>
-            Filter
+            {t('stepInfoFilter')}
             <div className='stepInfoContent'>
-              By default, the Data Explorer displays all available datasets.
-              Use the filters at the top of the screen to narrow down your selections
+              {t('stepInfoFilterText')}
             </div>
           </div>
         )
       case 'select':
         return (
           <div className='stepInfo'>
-            Select
+            {t('stepInfoSelect')}
             <div className='stepInfoContent'>
-              There are four ways to select data:
+              {t('stepInfoSelectTextA')}
+              {/* There are four ways to select data: */}
               <ul>
                 <li>
-                  Creating a rectangle at any zoom level:
+                  {t('stepInfoSelectTextB')}
+                  {/* Creating a rectangle at any zoom level:
                   Click and hold shift while dragging the cursor at any zoom level or use the rectangle tool (insert icon).
                   With the tool selected, click on the map to start drawing the rectangle.
                   Drag to the opposing corner and click again.
-                  Note that you can only have one rectangle selection active at a time.
+                  Note that you can only have one rectangle selection active at a time. */}
                 </li>
                 <li>
-                  Creating a polygon at any zoom level:
+                  {t('stepInfoSelectTextC')}
+                  {/* Creating a polygon at any zoom level:
                   For an irregular shape use the polygon tool (insert icon).
                   With the tool selected, click on the map to start drawing the polygon.
-                  When you\'ve finished drawing the search area, finish your search area by clicking on the first point again.
+                  When you\'ve finished drawing the search area, finish your search area by clicking on the first point again. */}
                 </li>
                 <li>
-                  Selecting points at the points zoom level:
-                  At the points zoom level, click to directly select.
+                  {t('stepInfoSelectTextD')}
+                  {/* Selecting points at the points zoom level:
+                  At the points zoom level, click to directly select. */}
                 </li>
               </ul>
             </div>
@@ -56,25 +58,30 @@ export default function IntroModal({ intialOpenState }) {
       case 'inspect':
         return (
           <div className='stepInfo'>
-            Inspect
+            {t('stepInfoInspect')}
             <div className='stepInfoContent'>
-              After making a selection, a summary of the selected dataset(s) will appear in a panel to the left.
-              Click the table headers to sort results alphabetically by title, type of dataset, or numerically by number of records and estimated size.
-              You can view each dataset in more detail by clicking the arrow button next to the estimated dataset size.
-              <i> Note: Only selected datasets will be available to download.</i>
+              {t('stepInfoInspectText')}
+
+              {/* <Trans i18nKey="stepInfoInspectText" >
+                After making a selection, a summary of the selected dataset(s) will appear in a panel to the left.
+                Click the table headers to sort results alphabetically by title, type of dataset, or numerically by number of records and estimated size.
+                You can view each dataset in more detail by clicking the arrow button next to the estimated dataset size.
+                <i> Note: Only selected datasets will be available to download.</i>
+              </Trans> */}
             </div>
           </div>
         )
       case 'download':
         return (
           <div className='stepInfo'>
-            Download
+            {t('stepInfoDownload')}
             <div className='stepInfoContent'>
-              To download the data, click the Download link at the bottom of the panel or top of the page.
+              {t('stepInfoDownloadText')}
+              {/* To download the data, click the Download link at the bottom of the panel or top of the page.
               This page allows the user to confirm their data order by verifying dataset titles, records, the size of the download, and inspect dataset details.
               Enter your email address and submit to confirm your order.
               An email providing the direct download link will be sent shortly.
-              <i> Note: Filters that have been applied in the search will also be applied to the data download. A 100MB size limit applies to all orders; downloads that are more than 100MB will be cut off at 100MB. Check the bar at the bottom of the panel to verify the order does not exceed the maximum. If the size is over 100MB, please submit multiple orders of smaller sizes.</i>
+              <i> Note: Filters that have been applied in the search will also be applied to the data download. A 100MB size limit applies to all orders; downloads that are more than 100MB will be cut off at 100MB. Check the bar at the bottom of the panel to verify the order does not exceed the maximum. If the size is over 100MB, please submit multiple orders of smaller sizes.</i> */}
             </div>
           </div>
         )
@@ -83,11 +90,13 @@ export default function IntroModal({ intialOpenState }) {
           <div className='tipInfo'>
             <p>
               <img className='feedbackButtonImage' />
-              Please fill out our user feedback survey! It helps improve this interface and find bugs.
+              {t('tipInfoFeedback')}
+              {/* Please fill out our user feedback survey! It helps improve this interface and find bugs. */}
             </p>
             <p>
               <img className='infoButtonImage' />
-              Click the info button to reopen this information panel.
+              {t('tipInfoReopen')}
+              {/* Click the info button to reopen this information panel. */}
             </p>
           </div>
         )
@@ -104,20 +113,23 @@ export default function IntroModal({ intialOpenState }) {
         onHide={() => setShowModal(false)}
         scrollable
         className='introModal'
-      // dialogClassName="modal-90w"
       >
         <Modal.Header closeButton>
           <Modal.Title className='modalHeader' id='contained-modal-title-vcenter'>
             <span>
-              CIOOS Data Explorer
+              {t('CIOOSDataExplorer')}
+              {/* CIOOS Data Explorer */}
             </span>
-            <span className='tagLine'> "Ocean Data For Our Ocean Future" </span>
-            <a title='Go to CIOOS homepage' className='introLogo' href='https://cioos.ca/' target='_blank' />
+            <span className='tagLine'>
+              {t('CIOOSQuote')}
+              {/* "Ocean Data For Our Ocean Future" */}
+            </span>
+            <a title={t('CIOOSLogoButtonTitle')} className='introLogo' href='https://cioos.ca/' target='_blank' />
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Container fluid>
-            {t('test', { test })}
+            {/* {t('test', { test })} */}
             <Row>
               <div
                 className='steps'
@@ -127,25 +139,25 @@ export default function IntroModal({ intialOpenState }) {
                   className='stepImage filterStep'
                   onMouseOver={() => setHoveredStep('filter')}
                 >
-                  Filter
+                  {t('stepInfoFilter')}
                 </div>
                 <div
                   className='stepImage selectStep'
                   onMouseOver={() => setHoveredStep('select')}
                 >
-                  Select
+                  {t('stepInfoSelect')}
                 </div>
                 <div
                   className='stepImage inspectStep'
                   onMouseOver={() => setHoveredStep('inspect')}
                 >
-                  Inspect
+                  {t('stepInfoInspect')}
                 </div>
                 <div
                   className='stepImage downloadStep'
                   onMouseOver={() => setHoveredStep('download')}
                 >
-                  Download
+                  {t('stepInfoDownload')}
                 </div>
               </div>
             </Row>
@@ -158,7 +170,7 @@ export default function IntroModal({ intialOpenState }) {
       <button
         className='introButton'
         onClick={() => setShowModal(true)}
-        title='Re-open introduction'
+        title={t('introReopenTitle')} //'Re-open introduction'
       >
         <InfoSquare color='#007bff' size={'25px'} />
       </button>
