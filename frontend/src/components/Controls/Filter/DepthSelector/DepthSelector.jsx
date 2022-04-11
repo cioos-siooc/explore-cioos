26729aa4ce380ff7cb73fb9ac66f49483c0c445e
JorinHakai <jorin.weatherston@hakai.org>
2022-04-11 15:31:36 -0700

    DepthSelector.jsx translated

--- frontend/src/components/Controls/Filter/DepthSelector/DepthSelector.jsx
+++ frontend/src/components/Controls/Filter/DepthSelector/DepthSelector.jsx
@@ -1,15 +1,18 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import RangeSelector from "../RangeSelector/RangeSelector.jsx";
+import * as React from "react"
+import { useState, useEffect } from "react"
+import PropTypes from "prop-types"
+import RangeSelector from "../RangeSelector/RangeSelector.jsx"
+import { useTranslation } from "react-i18next"
 
-import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
-import "rc-slider/assets/index.css";
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css"
+import "rc-slider/assets/index.css"
 
 import './styles.css'
-import { X } from "react-bootstrap-icons";
+import { X } from "react-bootstrap-icons"
 
 export default function DepthSelector(props) {
+  const { t } = useTranslation()
+
   const [startDepth, setStartDepth] = useState(props.startDepth)
   const [endDepth, setEndDepth] = useState(props.endDepth)
   const [depthValid, setDepthValid] = useState(true)
@@ -49,8 +52,8 @@ export default function DepthSelector(props) {
   return (
     <div className='depthSelector'>
       <div className='inputs'>
-        <div className='depth' title={!depthValid ? 'Start depth must be less than end depth, and be 0 to 12000m.' : undefined}>
-          <span>Start Depth (m):</span>
+        <div className='depth' title={!depthValid ? t('depthFilterStartInvalidTitle') : undefined}>
+          <span>{t('depthFilterStartDepth')}</span>
           <input
             className="startDepth"
             value={startDepth}
@@ -61,8 +64,8 @@ export default function DepthSelector(props) {
             size={"6"}
           />
         </div>
-        <div className='depth' title={!depthValid ? 'End depth must be more than start depth, and be 0 to 12000m' : undefined}>
-          <span>End Depth (m):</span>
+        <div className='depth' title={!depthValid ? t('depthFilterEndInvalidTitle') : undefined}>
+          <span>{t('depthFilterEndDepth')}</span>
           <input
             className="endDepth"
             value={endDepth}
@@ -80,8 +83,8 @@ export default function DepthSelector(props) {
         setStartDepth={props.setStartDepth}
         setEndDepth={props.setEndDepth}
       />
-      {!depthValid && (<div> <X color='red' size={30} /> Depth filter not applied, invalid values.</div>)}
-    </div>
+      {!depthValid && (<div> <X color='red' size={30} /> {t('depthFilterInvalidWarning')}</div>)}
+    </div >
   );
 }
 
