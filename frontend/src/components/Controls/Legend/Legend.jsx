c70ab19d2777f3607451770fad96e944f4e2d520
JorinHakai <jorin.weatherston@hakai.org>
2022-04-11 17:05:25 -0700

    legend.jsx translated

--- frontend/src/components/Controls/Legend/Legend.jsx
+++ frontend/src/components/Controls/Legend/Legend.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import { generateColorStops } from '../../../utilities.js'
 import { colorScale } from '../../config.js'
+import { useTranslation } from 'react-i18next'
 import * as _ from 'lodash'
 
 import './styles.css'
 
 export default function Legend({ currentRangeLevel }) {
+  const { t } = useTranslation()
   const colorStops = generateColorStops(colorScale, currentRangeLevel)
   return (currentRangeLevel[0] !== null && currentRangeLevel[1] !== null ?
     <div className='legend' >
@@ -17,7 +19,7 @@ export default function Legend({ currentRangeLevel }) {
               className='colorStop'
               key={index}
               style={{ 'backgroundColor': colorStop.color }}
-              title={`${colorStop.stop === 1 ? `${colorStop.stop} record` : `${colorStop.stop} records`}`}
+              title={`${colorStop.stop === 1 ? `${colorStop.stop} ${t('legendTitleText')}` : `${colorStop.stop} ${t('legendTitleTextPlural')}`}`}
             />
           )
         })
@@ -25,8 +27,12 @@ export default function Legend({ currentRangeLevel }) {
       {colorStops.length > 1 && currentRangeLevel[1]}
     </div>
     :
-    <div className='legend' title='Choose less restrictive filters to see data'>
-      No Data
+    <div
+      className='legend'
+      title={t('legendNoDataWarningTitle')} //'Choose less restrictive filters to see data'
+    >
+      {t('legendNoDataWarningText')}
+      {/* No Data */}
     </div>
   )
 }
