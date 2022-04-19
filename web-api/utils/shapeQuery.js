cba5689315dec147a645ee2e1a606296139885e0
Nate <nate.rosenstock@hakai.org>
2022-04-19 11:18:57 -0700

    add dataset summary in english, french where available

--- web-api/utils/shapeQuery.js
+++ web-api/utils/shapeQuery.js
@@ -28,6 +28,7 @@ async function getShapeQuery(query) {
         d.cdm_data_type,
         d.title title,
         json_build_object('en',title,'fr',title_fr) title_translated,
+        json_build_object('en',summary,'fr',summary_fr) summary_translated,
         d.ceda_eovs eovs,
         organizations,
         d.erddap_url || '/tabledap/' || d.dataset_id || '.html' as erddap_url,
