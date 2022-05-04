4bffd8226c57653187462898d1a868d0b132b6a9
Nate <nate.rosenstock@hakai.org>
2022-05-04 14:51:47 -0700

    fix formatting

--- web-api/routes/datasets.js
+++ web-api/routes/datasets.js
@@ -4,7 +4,12 @@ const db = require("../db");
 const cache = require("../utils/cache");
 
 router.get("/", cache.route(), async function (req, res, next) {
-  const SQL = "SELECT title, pk,organization_pks,json_build_object('en',title,'fr',title_fr) title_translated from cioos_api.datasets ORDER BY UPPER(title)"
+  const SQL = `SELECT title, 
+                      pk,
+                      organization_pks,
+                      json_build_object('en', title, 'fr', title_fr) title_translated
+                      FROM cioos_api.datasets
+                      ORDER BY UPPER(title)`
   
   res.send(
     (await db.raw(SQL)).rows
