da399fcf9db423a838c0f0d0e428a0ecf95221d0
Nate <nate.rosenstock@hakai.org>
2022-05-04 14:49:27 -0700

    add french dataset titles

--- web-api/routes/datasets.js
+++ web-api/routes/datasets.js
@@ -4,10 +4,10 @@ const db = require("../db");
 const cache = require("../utils/cache");
 
 router.get("/", cache.route(), async function (req, res, next) {
+  const SQL = "SELECT title, pk,organization_pks,json_build_object('en',title,'fr',title_fr) title_translated from cioos_api.datasets ORDER BY UPPER(title)"
+  
   res.send(
-    await db("cioos_api.datasets")
-      .select("title", "pk", "organization_pks")
-      .orderByRaw("UPPER(title)")
+    (await db.raw(SQL)).rows
   );
 });
 
