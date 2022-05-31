335b30128689bdd30e9dce923690c892575e77de
JorinHakai <jorin.weatherston@hakai.org>
2022-05-31 09:59:17 -0700

    Add scale control, legend style improvements, point selection opens new panel every selection.

--- frontend/src/components/Controls/Legend/LegendElement.jsx/LegendElement.jsx
+++ frontend/src/components/Controls/Legend/LegendElement.jsx/LegendElement.jsx
@@ -6,7 +6,7 @@ export default function LegendElement({ title = '', open, children }) {
   return (
     <div className='legendElement' title={capitalizeFirstLetter(title)}>
       {children}
-      {open && title && <>&nbsp;- {capitalizeFirstLetter(title)}</>}
+      {open && title && <>&nbsp;{capitalizeFirstLetter(title)}</>}
     </div>
   )
 }
