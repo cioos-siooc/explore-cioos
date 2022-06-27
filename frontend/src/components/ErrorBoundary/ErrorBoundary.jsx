50b6c6c285366318c4dbae96076f345050575583
JorinHakai <jorin.weatherston@hakai.org>
2022-06-27 15:13:05 -0700

    Added basic error boundary to app

---
+++ frontend/src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './styles.css'
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasError: false,
+      error: ''
+    }
+  }
+
+  static getDerivedStateFromError(error) {
+    return {
+      hasError: true,
+      error: error
+    }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className='errorBoundaryMessage'>
+          <img
+            className='errorLogo'
+            src={this.props.logoSource}
+            width='153px'
+            height='60px'
+          />
+          <h1>{this.props.errorBoundaryMessage}</h1>
+          {this.state.error.message}
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
