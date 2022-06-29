c6f54e8ad25baab54a6fe680d888d7baca9d70e9
Nate <nate.rosenstock@hakai.org>
2022-06-29 12:57:10 -0700

    add test to ensure API returns data for all endpoints

---
+++ test/test_api_endpoints.js
@@ -0,0 +1,32 @@
+import assert from "assert";
+import fetch from "node-fetch";
+/***
+ *
+ * Check that the API returns data for all endpoints
+ *
+ */
+
+// url of frontend
+const API_URL = "http://localhost:8098/api";
+
+const cdeQuery = (url) => fetch(API_URL + url).then((res) => res.json());
+
+(async () => {
+  const datasets = await cdeQuery("/datasets");
+  const legend = await cdeQuery("/legend");
+  const oceanVariables = await cdeQuery("/oceanVariables");
+  const organizations = await cdeQuery("/organizations");
+  const tiles = await fetch(API_URL + "/tiles/2/1/1.mvt").then((tile) =>
+    tile.arrayBuffer()
+  );
+  const pointQuery = await cdeQuery(
+    "/pointQuery?latMin=49.3194&lonMin=-123.7531&latMax=49.3552&lonMax=-123.6982"
+  );
+
+  assert(datasets.length == 1, "datasets");
+  assert(Object.values(legend.recordsCount).length == 3, "legend");
+  assert(organizations.length == 1, "organizations");
+  assert(oceanVariables.length == 1), "oceanVariables";
+  assert(pointQuery.length == 1), "pointQuery";
+  assert(tiles.byteLength > 2000), "tiles";
+})();
