6b980907bcb8e881263722fd56c0832684589f38
Nate <nate.rosenstock@hakai.org>
2022-06-30 09:51:03 -0700

    change all CEDA to CDE

--- downloader/test_downloader.sh
+++ downloader/test_downloader.sh
@@ -3,7 +3,7 @@ set -e
 
 # This assumes your Python evironment is setup.
 # If it isn't, you can also run these tests with 'docker build .' from the parent directory
-rm -rf ceda_download_*
+rm -rf cde_download_*
 for test_file in test/*.json; do
     echo $test_file
     python3 -m erddap_downloader $test_file
@@ -12,7 +12,7 @@ for test_file in test/*.json; do
         ls
         # Verify that a real CSV was created
         python3 -c "import pandas,glob; \
-                csv_file=glob.glob('ceda_download*/*.csv')[0]; \
+                csv_file=glob.glob('cde_download*/*.csv')[0]; \
                 print('Loading csv_file:',csv_file); \
                 df=pandas.read_csv(csv_file); \
                 print(df)"
--- recreate_database.sh
+++ recreate_database.sh
@@ -1,7 +1,7 @@
 docker-compose down
 docker-compose rm
-docker image rm $(docker images --filter=reference='ceda_*')
-docker volume rm $(docker volume ls --filter name="ceda_")
+docker image rm $(docker images --filter=reference='cde_*')
+docker volume rm $(docker volume ls --filter name="cde_")
 
 # and then restart the db:
 docker-compose up -d db
