40cdf80690d90689bc2746fe2c46f28575ca0ad8
Nate <nate.rosenstock@hakai.org>
2022-06-30 15:38:40 -0700

    remove unneeded rm

--- data_loader.sh
+++ data_loader.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-rm -rf harvest
 python -m erddap_scraper https://data.cioospacific.ca/erddap,https://catalogue.hakai.org/erddap,https://www.smartatlantic.ca/erddap,https://cioosatlantic.ca/erddap,https://erddap.ogsl.ca/erddap,http://dap.onc.uvic.ca/erddap,https://seagull-erddap.glos.org/erddap
 python -m cde_db_loader harvest
