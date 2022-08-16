6a8ee6effc14e53ae9c4d9c35c30263925c75685
Nate <nate.rosenstock@hakai.org>
2022-08-15 17:41:25 -0700

    use parameters in all SQL queries
    fix

--- web-api/utils/dbFilter.js
+++ web-api/utils/dbFilter.js
@@ -1,5 +1,6 @@
 const { polygonJSONToWKT } = require("./polygon");
 const unique = (arr) => [...new Set(arr)];
+const db = require("../db");
 
 function createDBFilter(request) {
   let {
@@ -22,60 +23,83 @@ function createDBFilter(request) {
   } = request;
 
   const filters = [];
+  const parameters = {};
 
   if (eovs) {
-    const eovsCommaSeparatedString = unique(eovs.split(","))
-      .map((eov) => `'${eov}'`)
-      .join();
-
-    filters.push(`eovs && array[${eovsCommaSeparatedString}]`);
+    parameters["eovsCommaSeparatedString"] = unique(eovs.split(","));
+    filters.push(`eovs && :eovsCommaSeparatedString`);
   }
 
   if (platforms) {
-    const platformsCommaSeparatedString = unique(
-      platforms
-        .split(","))
-        .map((platform) => `'${platform}'`).join();
-
-    filters.push(`platform = any(array[${platformsCommaSeparatedString}])`);
+    parameters["platformsCommaSeparatedString"] = unique(platforms.split(","));
+    filters.push(`platform = any(:platformsCommaSeparatedString)`);
   }
 
-  if (timeMin) filters.push(`time_max >= '${timeMin}'::timestamptz`);
-  if (timeMax) filters.push(`time_min <= '${timeMax}'::timestamptz`);
+  if (timeMin) {
+    parameters["timeMin"] = timeMin;
+    filters.push(`time_max >= :timeMin::timestamptz`);
+  }
+  if (timeMax) {
+    parameters["timeMax"] = timeMax;
+    filters.push(`time_min <= :timeMax::timestamptz`);
+  }
 
   // This would be used if there was a rectangle selection for download
-  if (latMin) filters.push(`latitude >= '${latMin}'::double precision`);
-  if (latMax) filters.push(`latitude <= '${latMax}'::double precision`);
+  if (latMin) {
+    parameters["latMin"] = latMin;
+    filters.push(`latitude >= (:latMin)::double precision`);
+  }
+  if (latMax) {
+    parameters["latMax"] = latMax;
+    filters.push(`latitude <= (:latMax)::double precision`);
+  }
 
-  if (lonMin) filters.push(`longitude >= '${lonMin}'::double precision`);
-  if (lonMax) filters.push(`longitude <= '${lonMax}'::double precision`);
+  if (lonMin) {
+    parameters["lonMin"] = lonMin;
+    filters.push(`longitude >= (:lonMin)::double precision`);
+  }
+  if (lonMax) {
+    parameters["lonMax"] = lonMax;
+    filters.push(`longitude <= (:lonMax)::double precision`);
+  }
 
   // disabled until we get depth data into the database
-  if (depthMin) filters.push(`depth_max >= '${depthMin}'::integer`);
-  if (depthMax) filters.push(`depth_min <= '${depthMax}'::integer`);
-  
+  if (depthMin) {
+    parameters["depthMin"] = depthMin;
+    filters.push("depth_max >= (:depthMin)::integer");
+  }
+  if (depthMax) {
+    parameters["depthMax"] = depthMax;
+    filters.push(`depth_min <= (:depthMax)::integer`);
+  }
+
   if (datasetPKs) {
-    filters.push(`d.pk = ANY ('{${datasetPKs}}')`);
+    parameters["datasetPKs"] = datasetPKs.split(",");
+    filters.push("d.pk = ANY (:datasetPKs)");
   }
 
   if (pointPKs) {
-    filters.push(`point_pk = ANY ('{${pointPKs}}')`);
+    parameters["pointPKs"] = pointPKs;
+    filters.push("point_pk = ANY (:pointPKs)");
   }
 
   if (organizations) {
-    const organizationsString = organizations.split(",").map((e) => `${e}`);
-    filters.push(`organization_pks && array[${organizationsString}]`);
+    parameters["organizationsString"] = organizations.split(",");
+    filters.push(`organization_pks && :organizationsString`);
   }
 
   if (polygon) {
     const wktPolygon = polygonJSONToWKT(polygon);
-
+    parameters["wktPolygon"] = wktPolygon;
     filters.push(
-      `ST_Contains(ST_GeomFromText('${wktPolygon}',4326),ST_Transform(geom,4326)) is true`
+      `ST_Contains(ST_GeomFromText(:wktPolygon,4326),ST_Transform(geom,4326)) is true`
     );
   }
+  const sql = filters.join(" AND \n");
 
-  return filters.join(" AND \n");
+  const query = db.raw(sql, parameters);
+
+  return query;
 }
 
 module.exports = createDBFilter;
