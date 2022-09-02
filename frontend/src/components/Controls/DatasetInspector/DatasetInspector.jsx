49affae18123791994607a85a9d72c137e48f415
Jorin Weatherston <jorin.weatherston@hakai.org>
2022-09-01 17:48:41 -0700

    improved charts and tables in dataset preview

--- frontend/src/components/Controls/DatasetInspector/DatasetInspector.jsx
+++ frontend/src/components/Controls/DatasetInspector/DatasetInspector.jsx
@@ -1,98 +1,73 @@
 import * as React from 'react'
-import { ChevronCompactLeft, CircleFill } from 'react-bootstrap-icons'
-import { Container, Table } from 'react-bootstrap'
+import { CircleFill } from 'react-bootstrap-icons'
+import { Container, Table, Col, Row } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 
 import platformColors from '../../platformColors'
 import './styles.css'
 
-export default function DatasetInspector({ dataset, setInspectDataset, setHoveredDataset, setInspectRecordID }) {
+export default function DatasetInspector({ dataset, setHoveredDataset, setInspectRecordID }) {
   const { t } = useTranslation()
   const platformColor = platformColors.filter(pc => pc.platform === dataset.platform)
   return (
-    <div className='datasetInspector'
-      onMouseEnter={() => setHoveredDataset(dataset)}
-      onMouseLeave={() => setHoveredDataset()}
-    >
-      <div
-        className='backButton'
-        onClick={() => setInspectDataset()}
-        title={t('datasetInspectorBackButtonTitle')} // 'Return to dataset list'
-      >
-        <ChevronCompactLeft />
-        {t('datasetInspectorBackButtonText')}
-        {/* Back */}
+    <div className='datasetInspector'>
+      <div className='datasetMetadata'>
+        <h5>
+          {t('datasetInspectorURLText')}
+          {/* Dataset URLs */}
+        </h5>
+        <div>
+          <ul style={{ listStyleType: 'none' }}>
+            <li>
+              <a
+                href={dataset.ckan_url}
+                target='_blank'
+                title={dataset.ckan_url ? dataset.ckan_url : 'unavailable'} rel="noreferrer">
+                {t('datasetInspectorCKANURL')} (CKAN)
+              </a>
+            </li>
+            <li>
+              <a
+                href={dataset.erddap_url}
+                target='_blank'
+                title={dataset.erddap_url ? dataset.erddap_url : 'unavailable'} rel="noreferrer">
+                {t('datasetInspectorERDDAPURL')} (ERDDAP)
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* <hr /> */}
+        <h5>
+          {t('datasetInspectorOrganizationText')}
+          {/* Organizations */}
+        </h5>
+        <div>
+          {dataset.organizations.join(', ')}
+        </div>
+        {/* <hr /> */}
+        <h5>
+          {t('datasetInspectorOceanVariablesText')}
+          {/* Ocean Variables */}
+        </h5>
+        <div>
+          {dataset.eovs.map((eov, index) => ' ' + t(eov)).join(',')}
+        </div>
+        {/* <hr /> */}
+        <h5>
+          {t('datasetInspectorPlatformText')}
+          {/* Platform */}
+        </h5>
+        <div>
+          {t(dataset.platform)}
+        </div>
+        {/* <hr /> */}
+        <h5>
+          {/* Records ... records total, 1000 shown */}
+          {t('datasetInspectorRecordsText')} ({dataset && dataset.profiles_count > 1000 ? `${dataset.profiles_count} ${t('datasetInspectorRecordsOverflowText')}` : dataset.profiles_count})
+        </h5>
       </div>
-      <div>
-        <Container style={{ pointerEvents: 'auto', margin: '10px 0px 10px 0px' }}>
-          <hr />
-          <h6>
-            {t('datasetInspectorTitleText')}
-            {/* Title */}
-          </h6>
-          <div>
-            <CircleFill className='optionColorCircle'
-              size='15'
-              fill={!_.isEmpty(platformColor) ? platformColor[0].color : '#000000'}
-            />
-            {dataset.title}
-          </div>
-          <hr />
-          <h6>
-            {t('datasetInspectorURLText')}
-            {/* Dataset URLs */}
-          </h6>
-          <div>
-            <ul style={{ listStyleType: 'none' }}>
-              <li>
-                <a
-                  href={dataset.ckan_url}
-                  target='_blank'
-                  title={dataset.ckan_url ? dataset.ckan_url : 'unavailable'} rel="noreferrer">
-                  {t('datasetInspectorCKANURL')} (CKAN)
-                </a>
-              </li>
-              <li>
-                <a
-                  href={dataset.erddap_url}
-                  target='_blank'
-                  title={dataset.erddap_url ? dataset.erddap_url : 'unavailable'} rel="noreferrer">
-                  {t('datasetInspectorERDDAPURL')} (ERDDAP)
-                </a>
-              </li>
-            </ul>
-          </div>
-          <hr />
-          <h6>
-            {t('datasetInspectorOrganizationText')}
-            {/* Organizations */}
-          </h6>
-          <div>
-            {dataset.organizations.join(', ')}
-          </div>
-          <hr />
-          <h6>
-            {t('datasetInspectorOceanVariablesText')}
-            {/* Ocean Variables */}
-          </h6>
-          <div>
-            {dataset.eovs.map((eov, index) => ' ' + t(eov)).join(',')}
-          </div>
-          <hr />
-          <h6>
-            {t('datasetInspectorPlatformText')}
-            {/* Platform */}
-          </h6>
-          <div>
-            {t(dataset.platform)}
-          </div>
-          <hr />
-          <h6>
-            {/* Records ... records total, 1000 shown */}
-            {t('datasetInspectorRecordsText')} ({dataset && dataset.profiles_count > 1000 ? `${dataset.profiles_count} ${t('datasetInspectorRecordsOverflowText')}` : dataset.profiles_count})
-          </h6>
-        </Container>
-        <Table className='inspectorTable' striped bordered size="sm">
+      <div className='inspectorTable'>
+        <Table striped bordered size="sm">
           <thead>
             <tr>
               <th>{t('datasetInspectorRecordIDText')}</th>
@@ -119,7 +94,7 @@ export default function DatasetInspector({ dataset, setInspectDataset, setHovere
             )}
           </tbody>
         </Table>
-      </div >
-    </div >
+      </div>
+    </div>
   )
 }
