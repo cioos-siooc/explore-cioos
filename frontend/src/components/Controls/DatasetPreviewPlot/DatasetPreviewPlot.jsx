18553c023bb9981ca9142e7030c189128f1c61c6
Jorin Weatherston <jorin.weatherston@hakai.org>
2022-09-06 11:05:46 -0700

    layout WIP

--- frontend/src/components/Controls/DatasetPreviewPlot/DatasetPreviewPlot.jsx
+++ frontend/src/components/Controls/DatasetPreviewPlot/DatasetPreviewPlot.jsx
@@ -10,12 +10,12 @@ export default function DatasetPreviewPlot({ datasetPreview, plotXAxis, setPlotX
         ?
         <>
           <DropdownButton title={(plotXAxis && `X axis: ` + plotXAxis.columnName) || 'Select X axis variable'}>
-            {datasetPreview && datasetPreview.table.columnNames.map((columnName, index) => {
+            {datasetPreview && datasetPreview?.table?.columnNames.map((columnName, index) => {
               return <Dropdown.Item key={index} onClick={() => setPlotXAxis({ index, columnName })}>{columnName}</Dropdown.Item>
             })}
           </DropdownButton>
           <DropdownButton title={(plotYAxis && `Y Axis: ` + plotYAxis.columnName) || 'Select Y axis variable'}>
-            {datasetPreview && datasetPreview.table.columnNames.map((columnName, index) => {
+            {datasetPreview && datasetPreview?.table?.columnNames.map((columnName, index) => {
               return <Dropdown.Item key={index} onClick={() => setPlotYAxis({ index, columnName })}>{columnName}</Dropdown.Item>
             })}
           </DropdownButton>
@@ -29,10 +29,10 @@ export default function DatasetPreviewPlot({ datasetPreview, plotXAxis, setPlotX
             < Plot
               data={[
                 {
-                  x: [...datasetPreview.table.rows.map((row) => {
+                  x: [...datasetPreview?.table?.rows.map((row) => {
                     return row[plotXAxis.index]
                   })] || [],
-                  y: [...datasetPreview.table.rows.map((row) => {
+                  y: [...datasetPreview?.table?.rows.map((row) => {
                     return row[plotYAxis.index]
                   })] || [],
                   type: 'scatter',
