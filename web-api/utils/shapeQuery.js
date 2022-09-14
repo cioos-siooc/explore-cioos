72aa89be6934df5baab4a9cddebe59fa26961ca2
Nate <nate.rosenstock@hakai.org>
2022-09-14 14:36:45 -0700

    datasetRecordsList bug fixes

--- web-api/utils/shapeQuery.js
+++ web-api/utils/shapeQuery.js
@@ -19,6 +19,7 @@ async function getShapeQuery(query,doEstimate=true,getRecordsList=true) {
                   json_build_object('en',summary,'fr',summary_fr) summary_translated,
                   d.eovs                                          eovs,
                   organizations,
+                  count(p.*) profiles_count,
                   d.erddap_url
                            || '/tabledap/'
                            || d.dataset_id
@@ -33,12 +34,12 @@ async function getShapeQuery(query,doEstimate=true,getRecordsList=true) {
                   coalesce(nullif(date_part('days',range_intersection_length(tstzrange(:timeMin,:timeMax),tstzrange(p.time_min,p.time_max))),0),1) * p.records_per_day *
                   -- depth multiplier - fraction of depth range that this query overlaps with profile depth range
                   coalesce(nullif(range_intersection_length(numrange(:depthMin,:depthMax),numrange(p.depth_min::NUMERIC,p.depth_max::NUMERIC)),0),1) / (coalesce(nullif(p.depth_max-p.depth_min,0),1)) ) AS records_count` : ""}
-                  ${getRecordsList ? `,json_agg(json_build_object( 'profile_id',coalesce(p.profile_id, p.timeseries_id), 'time_min',p.time_min::DATE, 'time_max',p.time_max::DATE, 'depth_min',p.depth_min, 'depth_max',p.depth_max ) ORDER BY time_min DESC ) AS profiles`: ""}
+                  ${getRecordsList ? `,json_agg(json_build_object( 'profile_id',coalesce(p.profile_id, p.timeseries_id), 'time_min',p.time_min::DATE, 'time_max',p.time_max::DATE, 'depth_min',p.depth_min, 'depth_max',p.depth_max ) ORDER BY time_min DESC ) AS profiles` : ""}
+                  
          FROM     cde.profiles p
          JOIN     cde.datasets d
          ON       p.dataset_pk = d.pk
-         WHERE    :filters
-                  -- AND ckan_record IS NOT NULL
+         WHERE :filters
          GROUP BY d.pk)
 SELECT *
        ${doEstimate ? `,round(:adder + records_count * num_columns * :multiplier) AS SIZE`: ''}
@@ -53,7 +54,8 @@ FROM   sub`;
       adder:0,
       multiplier:10,
     }
-  } else queryParams = filters
+  } else queryParams = { filters }
+  if (!queryParams.filters?.sql) queryParams.filters='TRUE'
 
   const q = db.raw(sql, queryParams);
 
