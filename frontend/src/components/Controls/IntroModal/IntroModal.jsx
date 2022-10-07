517e89315bc26b79511717686e507c1291a8c62e
JorinHakai <jorin.weatherston@hakai.org>
2022-10-07 11:22:05 -0700

    cleanup

--- frontend/src/components/Controls/IntroModal/IntroModal.jsx
+++ frontend/src/components/Controls/IntroModal/IntroModal.jsx
@@ -1,29 +1,14 @@
 import * as React from 'react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Container, Modal, Row } from 'react-bootstrap'
 import { ChatDots } from 'react-bootstrap-icons'
 import { useTranslation } from 'react-i18next'
-import { getCookieValue } from '../../../utilities'
 import './styles.css'
 
 export default function IntroModal({ showModal, setShowModal }) {
   const { t, i18n } = useTranslation()
-  // const introOpenCookie = !getCookieValue('introModalOpen')
-  // const [showModal, setShowModal] = useState(initialOpenState)
-  //   introOpenCookie !== undefined ? introOpenCookie : initialOpenState
-  // )
-
   const [hoveredStep, setHoveredStep] = useState()
   // Potential idea for cut through transparency to highligh controls: https://ishadeed.com/article/thinking-about-the-cut-out-effect/
-
-  // useEffect(() => {
-
-  // }, [showIntro])
-  // useEffect(() => {
-  //   if (showModal === false) {
-  //     document.cookie = `introModalOpen=${showModal}; Secure; max-age=${60 * 60 * 24 * 31}`
-  //   }
-  // }, [showModal])
 
   function generateInfo() {
     switch (hoveredStep) {
@@ -203,13 +188,6 @@ export default function IntroModal({ showModal, setShowModal }) {
           </Container>
         </Modal.Body>
       </Modal>
-      {/* <button
-        className='introButton'
-        onClick={() => setShowModal(true)}
-        title={t('introReopenTitle')} // 'Re-open introduction'
-      >
-        <InfoSquare color='#007bff' size={'25px'} />
-      </button> */}
     </div>
   )
 }
