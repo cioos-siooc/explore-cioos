4920a63bdb10895c46c08c0d82c0f294b43d72fe
Nate <nate.rosenstock@hakai.org>
2023-03-13 09:02:01 -0700

    clear hexes tables on harvest

--- database/6_remove_all_data.sql
+++ database/6_remove_all_data.sql
@@ -14,6 +14,8 @@ DELETE FROM cde.datasets;
 DELETE FROM cde.organizations;
 DELETE FROM cde.points;
 DELETE FROM cde.skipped_datasets;
+DELETE FROM cde.hexes_zoom_0;
+DELETE FROM cde.hexes_zoom_1;
 
 END;
 $$ LANGUAGE plpgsql;
