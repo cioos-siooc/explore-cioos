489fa76aead70cdeea6a5429e53a9e409c409b82
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-02-10 15:44:58 -0500

    remove default API_URL and make ARG required

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,8 @@
-const server = process.env.API_URL || 'https://pac-dev2.cioos.org/ceda/api'
+const server = process.env.API_URL
+// if no server is defined, raise an error
+console.debug('API_URL:', server)
+if (!server) {
+  throw new Error('API_URL is not defined')
+}
 
 module.exports = { server }
