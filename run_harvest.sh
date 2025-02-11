aee38e38d37da348dd05d462f31bbca34ab7df90
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-02-11 13:28:52 -0500

    drop old script used to run harvester from a different machine

--- run_harvest.sh
+++
@@ -1,6 +0,0 @@
-cd ~/cde
-rm -rf harvester_cache
-rm -rf ckan_harvester_cache
-~/miniconda3/envs/cde/bin/python -m cde_harvester -f harvest_config.yaml && \
-~/miniconda3/envs/cde/bin/python -m cde_db_loader && \
-ssh cioos@pac-prod2.cioos.org bash -c "cd ~/cde;sh /home/cioos/cde/post_harvest.sh"
