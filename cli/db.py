f44f62cf605050251339b9b257a0bec843fe5473
RichardKelly84 <richard.kelly@cioos.ca>
2025-08-20 14:22:28 -0230

    Updated formating and lint using ruff

--- cli/db.py
+++ cli/db.py
@@ -12,20 +12,21 @@ envs = os.environ
 database_link = f"postgresql://{envs['DB_USER']}:{envs['DB_PASSWORD']}@{envs['DB_HOST_EXTERNAL']}:{envs.get('DB_PORT', 5432)}/{envs['DB_NAME']}"
 engine = create_engine(database_link)
 
+
 def handle_error(e, verbose):
     try:
-        raise(e)
+        raise (e)
     except SQLAlchemyError as e:
         if verbose:
             print("\n❌ SQLAlchemy error occurred:", str(e))
         else:
             print("\n❌ SQLAlchemy error occurred:", str(e.orig))
     except Exception as e:
-        
         print("\n❌ Unexpected error occurred:", str(e))
 
         if verbose:
             traceback.print_exc()
+
 
 def drop_db(verbose):
     with engine.begin() as conn:
@@ -34,11 +35,13 @@ def drop_db(verbose):
             conn.execute(text("DROP SCHEMA IF EXISTS cde CASCADE;"))
             print("✅ Schema cde dropped             ")
         except Exception as e:
-            handle_error(e,verbose)
-@app.command(help ="initialize the database including all cde structures"  )
+            handle_error(e, verbose)
+
+
+@app.command(help="initialize the database including all cde structures")
 def init(verbose: bool = typer.Option(False, help="Enable verbose output.")):
     print(f"Initializing database: {envs['DB_NAME']}@{envs['DB_HOST_EXTERNAL']}")
-    
+
     try:
         engine = create_engine(database_link)
         with engine.begin() as conn:
@@ -49,7 +52,7 @@ def init(verbose: bool = typer.Option(False, help="Enable verbose output.")):
             print("🔄 Creating ckan_process() function", end="\r")
             conn.execute(text(sqlscripts.ckan_process))
             print("✅ ckan_process() created.           ")
-            
+
             print("🔄 Creating create_hexes() funtion", end="\r")
             conn.execute(text(sqlscripts.create_hexes))
             print("✅ create_hexes() created.           ")
@@ -57,12 +60,15 @@ def init(verbose: bool = typer.Option(False, help="Enable verbose output.")):
             print("🔄 Creating profile_process() funtion", end="\r")
             conn.execute(text(sqlscripts.profile_process))
             print("✅ profile_process() created.           ")
-            
+
             print("🔄 Creating remove_all_data() funtion", end="\r")
             conn.execute(text(sqlscripts.remove_all_data))
             print("✅ remove_all_data() created.           ")
 
-            print("🔄 Creating set_constraints() and drop_constraints() funtions", end="\r")
+            print(
+                "🔄 Creating set_constraints() and drop_constraints() funtions",
+                end="\r",
+            )
             conn.execute(text(sqlscripts.constraints))
             print("✅ set_constraints() and drop_constraints() created.           ")
 
@@ -72,10 +78,19 @@ def init(verbose: bool = typer.Option(False, help="Enable verbose output.")):
     except Exception as e:
         handle_error(e, verbose)
 
-@app.command(help = "Truncates all tables")
-def removeData(    verbose: bool = typer.Option(False, help="Enable verbose output."), y: bool = typer.Option(False, "--yes", "-y", help="Skip confirmation prompts and proceed with data but keeps database structure.")):
-    if (not y):
-        if(typer.confirm("This will clear all CDE data. Do you want to continue?")):
+
+@app.command(help="Truncates all tables")
+def removeData(
+    verbose: bool = typer.Option(False, help="Enable verbose output."),
+    y: bool = typer.Option(
+        False,
+        "--yes",
+        "-y",
+        help="Skip confirmation prompts and proceed with data but keeps database structure.",
+    ),
+):
+    if not y:
+        if typer.confirm("This will clear all CDE data. Do you want to continue?"):
             with engine.begin() as conn:
                 try:
                     print("🔄 Truncating Tables", end="\r")
@@ -86,32 +101,35 @@ def removeData(    verbose: bool = typer.Option(False, help="Enable verbose outp
     else:
         with engine.begin() as conn:
             try:
-                    print("🔄 Truncating Tables", end="\r")
-                    conn.execute(text("SELECT remove_all_data();"))
-                    print("✅ Tables Truncated            ")
+                print("🔄 Truncating Tables", end="\r")
+                conn.execute(text("SELECT remove_all_data();"))
+                print("✅ Tables Truncated            ")
             except Exception as e:
                 handle_error(e, verbose)
 
 
-@app.command(help="Clean the CDE database by dropping all structures. Optionally reinitialize the database.")
-
+@app.command(
+    help="Clean the CDE database by dropping all structures. Optionally reinitialize the database."
+)
 def clean(
     verbose: bool = typer.Option(False, help="Enable verbose output."),
-    y: bool = typer.Option(False, "--yes", "-y", help="Skip confirmation prompts and proceed with cleaning and reinitialization.")
+    y: bool = typer.Option(
+        False,
+        "--yes",
+        "-y",
+        help="Skip confirmation prompts and proceed with cleaning and reinitialization.",
+    ),
 ):
-
-    if (not y):
-        if(typer.confirm("This will clear all CDE data including all structures. Do you want to continue?")):
+    if not y:
+        if typer.confirm(
+            "This will clear all CDE data including all structures. Do you want to continue?"
+        ):
             drop_db(verbose)
-            if(typer.confirm("Do you want to reinitialize the database?")):
+            if typer.confirm("Do you want to reinitialize the database?"):
                 init(verbose)
     else:
         drop_db(verbose)
         init(verbose)
-        
-    
-
-
 
 
 if __name__ == "__main__":
--- cli/sqlscripts.py
+++ cli/sqlscripts.py
@@ -1,32 +1,29 @@
 class sqlscripts:
-
     folder = "../database/"
-    
+
     try:
-        with open(folder+"1_schema.sql","r") as scriptfile:
+        with open(folder + "1_schema.sql", "r") as scriptfile:
             schema = scriptfile.read()
-              
-        with open(folder+"3_ckan_process.sql") as scriptfile:
+
+        with open(folder + "3_ckan_process.sql") as scriptfile:
             ckan_process = scriptfile.read()
 
-        with open(folder+"4_create_hexes.sql") as scriptfile:
+        with open(folder + "4_create_hexes.sql") as scriptfile:
             create_hexes = scriptfile.read()
-        
-        with open(folder+"5_profile_process.sql") as scriptfile:
+
+        with open(folder + "5_profile_process.sql") as scriptfile:
             profile_process = scriptfile.read()
 
-        with open(folder+"6_remove_all_data.sql") as scriptfile:
+        with open(folder + "6_remove_all_data.sql") as scriptfile:
             remove_all_data = scriptfile.read()
-        
-        with open(folder+"7_contraints.sql") as scriptfile:
-            constraints = scriptfile.read()
-        
-        with open(folder+"8_range_functions.sql") as scriptfile:
-            range_functions = scriptfile.read()      
 
+        with open(folder + "7_contraints.sql") as scriptfile:
+            constraints = scriptfile.read()
+
+        with open(folder + "8_range_functions.sql") as scriptfile:
+            range_functions = scriptfile.read()
 
     except FileNotFoundError:
         print("Error: The file 'your_file.txt' was not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
-    
