c6b38031d287499d500995fa2ecd0f6685cf3477
Jessy Barrette <jessy.barrette@users.noreply.gitlab.com>
2025-09-25 10:26:56 -0400

    fix: update assertions in test_api_endpoints.js for consistency and clarity

--- test/test_api_endpoints.js
+++ test/test_api_endpoints.js
@@ -17,24 +17,30 @@ const cdeQuery = (url) => {
 
 (async () => {
   const datasets = await cdeQuery("/datasets");
+  assert(datasets.length == 2, "datasets.length != 2 ");
+  
   const legend = await cdeQuery("/legend");
+  assert(Object.values(legend.recordsCount).length == 3, "legend.length != 3 ");
+  
   const oceanVariables = await cdeQuery("/oceanVariables");
+  assert(oceanVariables.length == 1, "oceanVariables.length != 1: oceanVariables.length = " + oceanVariables.length);
+
   const organizations = await cdeQuery("/organizations");
+  assert(organizations.length == 2, "organizations.length != 2 ");
+  
   const tiles = await fetch(API_URL + "/tiles/2/1/1.mvt").then((tile) =>
     tile.arrayBuffer()
   );
+  assert(tiles.byteLength > 2000), "tiles.byteLength < 2000";
+
   const pointQuery = await cdeQuery(
     "/pointQuery?latMin=49.3194&lonMin=-123.7531&latMax=49.3552&lonMax=-123.6982"
   );
+  assert(pointQuery.length == 2, "pointQuery length != 2: pointQuery.length = " + pointQuery.length);
+  
   const preview = await cdeQuery(
     "/preview?profile=C44131&dataset=DFO_MEDS_BUOYS"
   );
-
-  assert(datasets.length == 2, "datasets.length != 2 ");
-  assert(Object.values(legend.recordsCount).length == 3, "legend.length != 3 ");
-  assert(organizations.length == 2, "organizations.length != 2 ");
-  assert(oceanVariables.length == 1, "oceanVariables.length != 1: oceanVariables.length = " + oceanVariables.length);
-  assert(pointQuery.length == 2, "pointQuery length != 1: pointQuery.length = " + pointQuery.length);
-  assert(tiles.byteLength > 2000), "tiles.byteLength < 2000";
   assert(preview.table.rows.length > 100), "preview.table.rows.length < 100";
+
 })();
