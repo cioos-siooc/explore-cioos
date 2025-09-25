a8cf8413cea78cece43c82afc96f04678ca7b937
Jessy Barrette <jessy.barrette@users.noreply.gitlab.com>
2025-09-25 12:27:59 -0400

    fix: enhance logging in frontend load test for better visibility

--- test/frontend_loads_without_errors.js
+++ test/frontend_loads_without_errors.js
@@ -12,6 +12,9 @@ const url = "http://localhost:8098";
 
 (async () => {
   const browser = await puppeteer.launch();
+  console.log("Testing frontend at ", url);
+
+  console.log("Opening page");
   const page = await browser.newPage();
   page.on("console", (msg) => {
     if (msg.type() === "error") {
@@ -19,18 +22,22 @@ const url = "http://localhost:8098";
     }
   });
 
+  console.log("Listening for errors on the page");
   page
     .on("pageerror", ({ message }) => errors.push(message))
     .on("requestfailed", (request) =>
       errors.push(`${request.failure().errorText} ${request.url()}`)
-    );
+  );
+  console.log("Going to ", url);
   await page.goto(url, {
     waitUntil: "domcontentloaded",
   });
+  console.log("Waiting for network to be idle");
   browser.close();
   if (errors.length) {
     console.log(errors);
     process.exit(1);
   }
+  console.log("No errors detected");
   process.exit(0);
 })();
