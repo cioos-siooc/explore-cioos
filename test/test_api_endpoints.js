7e720fe005660c0634dc29289502522c2476daba
Jessy Barrette <jessy.barrette@users.noreply.gitlab.com>
2025-09-25 10:27:50 -0400

    fix: improve logging for API endpoint tests for better clarity

--- test/test_api_endpoints.js
+++ test/test_api_endpoints.js
@@ -16,28 +16,37 @@ const cdeQuery = (url) => {
 };
 
 (async () => {
+  console.log("Testing API endpoints at ", API_URL);
+
+  console.log("Testing /datasets")
   const datasets = await cdeQuery("/datasets");
   assert(datasets.length == 2, "datasets.length != 2 ");
   
+  console.log("Testing /legend")
   const legend = await cdeQuery("/legend");
   assert(Object.values(legend.recordsCount).length == 3, "legend.length != 3 ");
   
+  console.log("Testing /organizations")
   const oceanVariables = await cdeQuery("/oceanVariables");
   assert(oceanVariables.length == 1, "oceanVariables.length != 1: oceanVariables.length = " + oceanVariables.length);
 
+  console.log("Testing /oceanVariables")
   const organizations = await cdeQuery("/organizations");
   assert(organizations.length == 2, "organizations.length != 2 ");
   
+  console.log("Testing /tiles")
   const tiles = await fetch(API_URL + "/tiles/2/1/1.mvt").then((tile) =>
     tile.arrayBuffer()
   );
   assert(tiles.byteLength > 2000), "tiles.byteLength < 2000";
 
+  console.log("Testing /pointQuery")
   const pointQuery = await cdeQuery(
     "/pointQuery?latMin=49.3194&lonMin=-123.7531&latMax=49.3552&lonMax=-123.6982"
   );
   assert(pointQuery.length == 2, "pointQuery length != 2: pointQuery.length = " + pointQuery.length);
   
+  console.log("Testing /preview")
   const preview = await cdeQuery(
     "/preview?profile=C44131&dataset=DFO_MEDS_BUOYS"
   );
