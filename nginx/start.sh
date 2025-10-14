3b97fb6e2dffabc26b0353eccf9bfe406f19e99f
Richard Kelly <Richard.Kelly@cioos.ca>
2025-10-14 13:53:56 -0230

    fixed permisions

--- nginx/start.sh
+++ nginx/start.sh
@@ -3,7 +3,8 @@
 # Start the cron service
 service cron start
 
-# Start the logrotate service
-logrotate /etc/logrotate.d/nginx
+# Ensure logrotate config has correct permissions
+chown root:root /etc/logrotate.conf
+chmod 0644 /etc/logrotate.conf
  
 nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
