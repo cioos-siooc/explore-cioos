11bbda6c0dd67dfb42eddebce7e835b608bba73f
Richard Kelly <Richard.Kelly@cioos.ca>
2026-04-08 12:41:53 -0230

    chore: remove migrations folder, full rebuild is the intended upgrade path

--- database/migrations/add_scientific_names.sql
+++
@@ -1,2 +0,0 @@
--- Add scientific_names column to profiles table for OBIS occurrence data
-ALTER TABLE cde.profiles ADD COLUMN IF NOT EXISTS scientific_names text[] DEFAULT '{}';
--- database/migrations/download-all-columns.sql
+++
@@ -1,5 +0,0 @@
-DROP VIEW cde.dataset_to_eov;
-DROP TABLE IF EXISTS cde.erddap_variables;
-DROP TABLE IF EXISTS cde.eov_to_standard_name;
-
-ALTER TABLE cde.datasets ADD COLUMN num_columns integer;
