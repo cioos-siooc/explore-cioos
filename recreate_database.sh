5564951a2b10a6ff8aedf819a83b2efbd4f52c9d
Richard Kelly <Richard.Kelly@cioos.ca>
2026-04-13 09:31:05 -0230

    fix: update recreate_database.sh for docker compose v2

--- recreate_database.sh
+++ recreate_database.sh
@@ -1,7 +1,7 @@
-docker-compose down
-docker-compose rm
-docker image rm $(docker images --filter=reference='cde_*')
-docker volume rm $(docker volume ls --filter name="cde_")
+docker compose down
+docker compose rm
+docker image rm $(docker images --filter=reference='cde-*' -q)
+docker volume rm $(docker volume ls --filter name="cde-" -q)
 
 # and then restart the db:
-docker-compose up -d db
+docker compose up -d db
