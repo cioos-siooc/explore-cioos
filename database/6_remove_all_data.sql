e32fea2ce97ad429379eb0eaffd31a50b14c13e7
Richard Kelly <Richard.Kelly@cioos.ca>
2026-04-16 13:42:52 -0230

    refactor: combine TRUNCATE statements in remove_all_data()

--- database/6_remove_all_data.sql
+++ database/6_remove_all_data.sql
@@ -9,14 +9,8 @@ Tables are truncated during data ingestion
 CREATE OR REPLACE FUNCTION remove_all_data() RETURNS VOID AS $$
 BEGIN
 
-  TRUNCATE cde.profiles;
-  TRUNCATE cde.obis_cells;
-  TRUNCATE cde.datasets;
-  TRUNCATE cde.organizations;
-  TRUNCATE cde.points;
-  TRUNCATE cde.skipped_datasets;
-  TRUNCATE cde.hexes_zoom_0;
-  TRUNCATE cde.hexes_zoom_1;
+  TRUNCATE cde.profiles, cde.obis_cells, cde.datasets, cde.organizations,
+           cde.points, cde.skipped_datasets, cde.hexes_zoom_0, cde.hexes_zoom_1;
 
 END;
 $$ LANGUAGE plpgsql;
