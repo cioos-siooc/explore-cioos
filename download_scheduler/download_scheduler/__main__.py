3efc16ad22f3cbc819160b2bd18ed75ba0b56208
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2026-04-23 15:20:24 -0400

    feat: implement health check endpoints and server for improved service monitoring

--- download_scheduler/download_scheduler/__main__.py
+++ download_scheduler/download_scheduler/__main__.py
@@ -3,8 +3,10 @@ import time
 from loguru import logger
 
 from download_scheduler.download_scheduler import get_a_download_job, run_download
+from download_scheduler.health_server import start_health_server
 
 if __name__ == "__main__":
+    start_health_server(port=8000)
     logger.debug("Waiting for jobs..")
     while True:
         row = get_a_download_job()
